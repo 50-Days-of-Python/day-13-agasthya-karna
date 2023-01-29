75b20df6936ae82a7032ca45d59950d56c11d74f
Agasthya Karnamadakala <104967395+agasthya-karna@users.noreply.github.com>
2023-01-29 08:52:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 #Write your code here.
+def calculate_vat(price, vat):
+    try:
+        price = float(price)
+        vat = float(vat)
+        total_price = price + price * (vat / 100)
+        return round(total_price)
+    except ValueError:
+        return "Invalid Input"
+
+price = input()
+vat = input()
+
+print(calculate_vat(price, vat))
